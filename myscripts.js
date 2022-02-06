fefb3caeb852157b67b9fab27fe7db39e4902719
VovaIzmailov <99125988+VovaIzmailov@users.noreply.github.com>
2022-02-06 15:42:53 +0200

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,10 @@
+// Ariel count
+var count = 0;
+var btn = document.getElementById("Abtn");
+var disp = document.getElementById("resultA");
+
+btn.onclick = function () {
+  count++;
+  disp.innerHTML = count;
+}
+// Gal count
